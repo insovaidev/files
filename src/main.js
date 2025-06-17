55e4b88809928c6780579f4eef3403f6a4f9c09a
insovai <insovaidev@gmail.com>
2025-06-17 11:00:59 +0700

    'update'

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import { createApp } from 'vue'
-import './index.css'
-import App from './App.vue'
-import store from "./store"
-import router from "./router"
-
-createApp(App).use(store).use(router).mount('#app')
-import "bootstrap/dist/js/bootstrap.js"
